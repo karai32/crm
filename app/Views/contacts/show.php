0ba8635d017176e9b3523a428ae2690ef4bdc964
Artem Tepliakov <1aszx12@mail.ru>
2026-06-22 16:38:32 +0200

    New icons.

--- app/Views/contacts/show.php
+++ app/Views/contacts/show.php
@@ -148,9 +148,7 @@ $canEditContacts = Auth::can('contacts.edit');
                 <div class="contact-sidebar-list">
                     <?php foreach ($clients as $client): ?>
                         <div class="contact-sidebar-item">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.6" stroke="currentColor" class="contact-sidebar-icon">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12"/>
-                            </svg>
+                            <i class="ph ph-buildings contact-sidebar-icon"></i>
                             <a href="<?= htmlspecialchars(Auth::url('/clients/show?id=' . $client['id']), ENT_QUOTES, 'UTF-8') ?>">
                                 <?= htmlspecialchars($client['commercial_name'], ENT_QUOTES, 'UTF-8') ?>
                             </a>
