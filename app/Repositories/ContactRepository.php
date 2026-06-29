8844983e5de45cbd4d4cbd48bfb7ffd9d3a69c5b
Artem Tepliakov <1aszx12@mail.ru>
2026-06-29 15:46:48 +0200

    contacts table upgrade.

--- app/Repositories/ContactRepository.php
+++ app/Repositories/ContactRepository.php
@@ -234,7 +234,7 @@ class ContactRepository
         $placeholders = implode(',', array_fill(0, count($contactIds), '?'));
 
         $statement = $pdo->prepare("
-            SELECT cc.contact_id, cl.id, cl.commercial_name, s.icon AS sector_icon
+            SELECT cc.contact_id, cl.id, cl.commercial_name, s.icon AS sector_icon, s.name AS sector_name
             FROM client_contacts cc
             INNER JOIN clients cl ON cl.id = cc.client_id
             LEFT JOIN sectors s ON s.id = cl.sector_id
