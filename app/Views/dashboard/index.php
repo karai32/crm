5cf097230a7f1738d8f7ec8dfdb1d0785248089a
Artem Tepliakov <1aszx12@mail.ru>
2026-06-29 16:06:46 +0200

    Recent contacts table upgrade.

--- app/Views/dashboard/index.php
+++ app/Views/dashboard/index.php
@@ -1,12 +1,13 @@
 <?php
-$avatarClasses = ['avatar-c1','avatar-c2','avatar-c3','avatar-c4','avatar-c5','avatar-c6','avatar-c7','avatar-c8'];
+$avatarClasses = ['avatar-c1', 'avatar-c2', 'avatar-c3', 'avatar-c4', 'avatar-c5', 'avatar-c6', 'avatar-c7', 'avatar-c8'];
 
 $maxSectorCount = 1;
 if (!empty($clientsBySector)) {
     $maxSectorCount = max(array_column($clientsBySector, 'clients_count')) ?: 1;
 }
 
-function fmtDate(string $date): string {
+function fmtDate(string $date): string
+{
     $ts = strtotime($date);
     return $ts ? date('M j, Y', $ts) : $date;
 }
@@ -71,59 +72,69 @@ function fmtDate(string $date): string {
         <?php if (empty($latestContacts)): ?>
             <div class="empty-state">No contacts yet.</div>
         <?php else: ?>
-        <div class="recent-table-wrap">
-        <table class="data-table recent-table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Company</th>
-                    <th>Clients</th>
-                    <th>Created</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($latestContacts as $contact): ?>
-                <tr>
-                    <td>
-                        <a class="contact-name-link"
-                           href="<?= htmlspecialchars(Auth::url('/contacts/show?id=' . $contact['id']), ENT_QUOTES, 'UTF-8') ?>">
-                            <?= htmlspecialchars($contact['full_name'] ?? '', ENT_QUOTES, 'UTF-8') ?>
-                        </a>
-                    </td>
-                    <td><?= htmlspecialchars($contact['email'] ?? '-', ENT_QUOTES, 'UTF-8') ?></td>
-                    <td>
-                        <span class="company-indicator">
-                            <span class="company-indicator-dot <?= ($contact['company'] ?? '') !== '' ? 'yes' : 'no' ?>"></span>
-                            <?= ($contact['company'] ?? '') !== '' ? htmlspecialchars($contact['company'], ENT_QUOTES, 'UTF-8') : 'No' ?>
-                        </span>
-                    </td>
-                    <td class="col-clients">
-                        <?php
-                            $cList  = $latestContactClients[(int) $contact['id']] ?? [];
-                            $cCount = count($cList);
-                        ?>
-                        <?php if ($cCount > 0): ?>
-                            <a class="col-client-link" href="<?= htmlspecialchars(Auth::url('/clients/show?id=' . (int) $cList[0]['id']), ENT_QUOTES, 'UTF-8') ?>">
-                                <?= htmlspecialchars($cList[0]['commercial_name'], ENT_QUOTES, 'UTF-8') ?>
-                            </a>
-                            <?php if ($cCount > 1):
-                                $moreUrl = Auth::url('/clients?' . http_build_query(['contact_name' => $contact['full_name']]));
-                            ?>
-                            <a class="col-clients-more" href="<?= htmlspecialchars($moreUrl, ENT_QUOTES, 'UTF-8') ?>">+<?= $cCount - 1 ?></a>
-                            <?php endif; ?>
-                        <?php endif; ?>
-                    </td>
-                    <td>
-                        <span class="created-date">
-                            <?= htmlspecialchars(!empty($contact['created_at']) ? fmtDate($contact['created_at']) : '-', ENT_QUOTES, 'UTF-8') ?>
-                        </span>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-        </div>
+            <div class="recent-table-wrap">
+                <table class="data-table recent-table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Company</th>
+                            <th>Clients</th>
+                            <th>Created</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($latestContacts as $contact): ?>
+                            <tr>
+                                <td>
+                                    <a class="contact-name-link"
+                                        href="<?= htmlspecialchars(Auth::url('/contacts/show?id=' . $contact['id']), ENT_QUOTES, 'UTF-8') ?>">
+                                        <?= htmlspecialchars($contact['full_name'] ?? '', ENT_QUOTES, 'UTF-8') ?>
+                                    </a>
+                                </td>
+                                <td><?= htmlspecialchars($contact['email'] ?? '-', ENT_QUOTES, 'UTF-8') ?></td>
+                                <td>
+                                    <span class="company-indicator">
+                                        <span class="company-indicator-dot <?= ($contact['company'] ?? '') !== '' ? 'yes' : 'no' ?>"></span>
+                                        <?= ($contact['company'] ?? '') !== '' ? htmlspecialchars($contact['company'], ENT_QUOTES, 'UTF-8') : 'No' ?>
+                                    </span>
+                                </td>
+                                <td class="col-clients">
+                                    <?php
+                                    $cList  = $latestContactClients[(int) $contact['id']] ?? [];
+                                    $cCount = count($cList);
+                                    ?>
+                                    <?php if ($cCount > 0): ?>
+                                        <span class="col-client-main">
+                                            <?php if (!empty($cList[0]['sector_icon'])): ?>
+                                                <span class="sector-list-icon">
+                                                    <i class="ph ph-<?= htmlspecialchars($cList[0]['sector_icon'], ENT_QUOTES, 'UTF-8') ?>"></i>
+                                                    <span class="tooltip-text"><?= htmlspecialchars($cList[0]['sector_name'], ENT_QUOTES, 'UTF-8') ?></span>
+                                                </span>
+                                            <?php endif; ?>
+
+                                            <a class="col-client-link" href="<?= htmlspecialchars(Auth::url('/clients/show?id=' . (int) $cList[0]['id']), ENT_QUOTES, 'UTF-8') ?>">
+                                                <?= htmlspecialchars($cList[0]['commercial_name'], ENT_QUOTES, 'UTF-8') ?>
+                                            </a>
+                                        </span>
+
+                                        <?php if ($cCount > 1):
+                                            $moreUrl = Auth::url('/clients?' . http_build_query(['contact_name' => $contact['full_name']]));
+                                        ?>
+                                            <a class="col-clients-more" href="<?= htmlspecialchars($moreUrl, ENT_QUOTES, 'UTF-8') ?>">+<?= $cCount - 1 ?></a>
+                                        <?php endif; ?>
+                                    <?php endif; ?>
+                                </td>
+                                <td>
+                                    <span class="created-date">
+                                        <?= htmlspecialchars(!empty($contact['created_at']) ? fmtDate($contact['created_at']) : '-', ENT_QUOTES, 'UTF-8') ?>
+                                    </span>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
         <?php endif; ?>
     </div>
 
@@ -139,20 +150,20 @@ function fmtDate(string $date): string {
             <?php if (empty($clientsBySector)): ?>
                 <div class="sector-empty">No sector data yet.</div>
             <?php else: ?>
-            <div class="sector-rows">
-                <?php foreach (array_slice($clientsBySector, 0, 8) as $row): ?>
-                <?php $pct = round(($row['clients_count'] / $maxSectorCount) * 100); ?>
-                <div class="sector-row">
-                    <span class="sector-row-name"><?= htmlspecialchars($row['sector_name'], ENT_QUOTES, 'UTF-8') ?></span>
-                    <div class="sector-row-bar-wrap">
-                        <div class="sector-row-bar">
-                            <div class="sector-row-bar-fill" style="width:<?= $pct ?>%"></div>
+                <div class="sector-rows">
+                    <?php foreach (array_slice($clientsBySector, 0, 8) as $row): ?>
+                        <?php $pct = round(($row['clients_count'] / $maxSectorCount) * 100); ?>
+                        <div class="sector-row">
+                            <span class="sector-row-name"><?= htmlspecialchars($row['sector_name'], ENT_QUOTES, 'UTF-8') ?></span>
+                            <div class="sector-row-bar-wrap">
+                                <div class="sector-row-bar">
+                                    <div class="sector-row-bar-fill" style="width:<?= $pct ?>%"></div>
+                                </div>
+                            </div>
+                            <span class="sector-row-count"><?= (int) $row['clients_count'] ?> clients</span>
                         </div>
-                    </div>
-                    <span class="sector-row-count"><?= (int) $row['clients_count'] ?> clients</span>
+                    <?php endforeach; ?>
                 </div>
-                <?php endforeach; ?>
-            </div>
             <?php endif; ?>
         </div>
 
@@ -166,30 +177,30 @@ function fmtDate(string $date): string {
                 <div class="empty-state">No clients yet.</div>
             <?php else: ?>
                 <?php foreach (array_slice($topClients, 0, 5) as $i => $client): ?>
-                <?php
+                    <?php
                     $initials = strtoupper(substr($client['commercial_name'], 0, 2));
                     $colorClass = $avatarClasses[$i % count($avatarClasses)];
-                ?>
-                <a class="top-client-item"
-                   href="<?= htmlspecialchars(Auth::url('/clients/show?id=' . $client['id']), ENT_QUOTES, 'UTF-8') ?>">
-                    <div class="client-avatar <?= $colorClass ?>">
-                        <?= htmlspecialchars($initials, ENT_QUOTES, 'UTF-8') ?>
-                    </div>
-                    <div class="client-meta">
-                        <div class="client-meta-name">
-                            <?= htmlspecialchars($client['commercial_name'], ENT_QUOTES, 'UTF-8') ?>
+                    ?>
+                    <a class="top-client-item"
+                        href="<?= htmlspecialchars(Auth::url('/clients/show?id=' . $client['id']), ENT_QUOTES, 'UTF-8') ?>">
+                        <div class="client-avatar <?= $colorClass ?>">
+                            <?= htmlspecialchars($initials, ENT_QUOTES, 'UTF-8') ?>
                         </div>
-                        <?php if (!empty($client['sector_name'])): ?>
-                        <div class="client-meta-sector">
-                            <?= htmlspecialchars($client['sector_name'], ENT_QUOTES, 'UTF-8') ?>
+                        <div class="client-meta">
+                            <div class="client-meta-name">
+                                <?= htmlspecialchars($client['commercial_name'], ENT_QUOTES, 'UTF-8') ?>
+                            </div>
+                            <?php if (!empty($client['sector_name'])): ?>
+                                <div class="client-meta-sector">
+                                    <?= htmlspecialchars($client['sector_name'], ENT_QUOTES, 'UTF-8') ?>
+                                </div>
+                            <?php endif; ?>
                         </div>
-                        <?php endif; ?>
-                    </div>
-                    <div class="client-count">
-                        <span class="client-count-num"><?= (int) $client['contacts_count'] ?></span>
-                        <span class="client-count-label">Contacts</span>
-                    </div>
-                </a>
+                        <div class="client-count">
+                            <span class="client-count-num"><?= (int) $client['contacts_count'] ?></span>
+                            <span class="client-count-label">Contacts</span>
+                        </div>
+                    </a>
                 <?php endforeach; ?>
 
                 <div class="card-footer">
